b70c114401d8a001cb8739669b971bf570e8fb93
federicocarreno <fedesud@gmail.com>
2020-09-20 01:14:20 -0300

    bugs in css and last updated

--- byui/webdev/src/script.js
+++ byui/webdev/src/script.js
@@ -1,4 +1,2 @@
 document.getElementById('actualyear').appendChild(document.createTextNode(new Date().getFullYear()))
-
-let lastupdate = new Date(document.lastModified);
-document.getElementById("lastupdate").textContent = lastupdate;
+document.getElementById("lastupdate").innerHTML = document.lastModified
