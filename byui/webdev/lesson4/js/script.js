bf69afa8ff92d8c2a120ff37a0ebce943bce9dfd
federicocarreno <fedesud@gmail.com>
2020-10-31 10:15:01 -0300

    test

--- byui/webdev/lesson4/js/script.js
+++ byui/webdev/lesson4/js/script.js
@@ -5,7 +5,3 @@ const mainnav = document.querySelector('.menu-list')
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-//footer dates
-document.getElementById('actualyear').appendChild(document.createTextNode(new Date().getFullYear()))
-document.getElementById("lastupdate").innerHTML = document.lastModified
